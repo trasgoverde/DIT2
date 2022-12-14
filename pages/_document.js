be73c42793be0a6abf62bea76ed74e9cdf091afc
Ignacio <ign.souto@gmail.com>
2022-12-14 14:46:08 +0100

    New Descrition

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <meta property="og:title" content="Dipassio | Smart Contracts Audit GPT-3" key="title"/>
-        <meta property="og:description" content="by Ignacio Souto" key="description"/>
+        <meta property="og:description" content="Dipass.io es una Consultora IT que provee servicios de transformación Digital, especializada en Blockchain y desarrollos Web3, que ayuda a las empresas a alcanzar sus objetivos de desarrollo digital mediante el desarrollo de vías web3, desarrollos de contratos inteligentes y auditorías para el cumplimiento normativo y la seguridad. Hacemos esto a través de una combinación de nuestros desarrolladores internos Full-Stack y Auditores de Contratos Inteligentes. It was created by &lt;a href=https://www.linkedin.com/company/dipassio/ Ignacio Souto&lt;/a&gt;." key="description"/>
+
       </Head>
       <body>
         <Main />
